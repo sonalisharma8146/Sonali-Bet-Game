b8e6445c3699f1693d7644fb0efa8bc1dc98a320
Akash sharma <as953866@gmail.com>
2020-07-20 12:20:28 +1200

    Add project files.

---
+++ BetGame2/HomeForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace BetGame2
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureCycle1 = new System.Windows.Forms.PictureBox();
+            this.pictureCycle2 = new System.Windows.Forms.PictureBox();
+            this.pictureCycle3 = new System.Windows.Forms.PictureBox();
+            this.pictureCycle4 = new System.Windows.Forms.PictureBox();
+            this.pictureTracks = new System.Windows.Forms.PictureBox();
+            this.panelBet = new System.Windows.Forms.Panel();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.punter1Radio = new System.Windows.Forms.RadioButton();
+            this.punter2Radio = new System.Windows.Forms.RadioButton();
+            this.punter3Radio = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxPunter1 = new System.Windows.Forms.TextBox();
+            this.textBoxPunter2 = new System.Windows.Forms.TextBox();
+            this.textBoxPunter3 = new System.Windows.Forms.TextBox();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.labelBet = new System.Windows.Forms.Label();
+            this.labelCycleNo = new System.Windows.Forms.Label();
+            this.numericBetAmount = new System.Windows.Forms.NumericUpDown();
+            this.numericCycleNo = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTracks)).BeginInit();
+            this.panelBet.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBetAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericCycleNo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureCycle1
+            // 
+            this.pictureCycle1.Image = global::BetGame2.Properties.Resources.cycle1;
+            this.pictureCycle1.Location = new System.Drawing.Point(12, 12);
+            this.pictureCycle1.Name = "pictureCycle1";
+            this.pictureCycle1.Size = new System.Drawing.Size(129, 66);
+            this.pictureCycle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureCycle1.TabIndex = 0;
+            this.pictureCycle1.TabStop = false;
+            // 
+            // pictureCycle2
+            // 
+            this.pictureCycle2.Image = global::BetGame2.Properties.Resources.cycle2;
+            this.pictureCycle2.Location = new System.Drawing.Point(12, 113);
+            this.pictureCycle2.Name = "pictureCycle2";
+            this.pictureCycle2.Size = new System.Drawing.Size(129, 66);
+            this.pictureCycle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureCycle2.TabIndex = 1;
+            this.pictureCycle2.TabStop = false;
+            // 
+            // pictureCycle3
+            // 
+            this.pictureCycle3.Image = global::BetGame2.Properties.Resources.cycle3;
+            this.pictureCycle3.Location = new System.Drawing.Point(12, 214);
+            this.pictureCycle3.Name = "pictureCycle3";
+            this.pictureCycle3.Size = new System.Drawing.Size(129, 66);
+            this.pictureCycle3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureCycle3.TabIndex = 2;
+            this.pictureCycle3.TabStop = false;
+            // 
+            // pictureCycle4
+            // 
+            this.pictureCycle4.Image = global::BetGame2.Properties.Resources.cycle4;
+            this.pictureCycle4.Location = new System.Drawing.Point(12, 315);
+            this.pictureCycle4.Name = "pictureCycle4";
+            this.pictureCycle4.Size = new System.Drawing.Size(129, 66);
+            this.pictureCycle4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureCycle4.TabIndex = 3;
+            this.pictureCycle4.TabStop = false;
+            // 
+            // pictureTracks
+            // 
+            this.pictureTracks.Image = global::BetGame2.Properties.Resources.tracks;
+            this.pictureTracks.Location = new System.Drawing.Point(147, 1);
+            this.pictureTracks.Name = "pictureTracks";
+            this.pictureTracks.Size = new System.Drawing.Size(1334, 406);
+            this.pictureTracks.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureTracks.TabIndex = 4;
+            this.pictureTracks.TabStop = false;
+            // 
+            // panelBet
+            // 
+            this.panelBet.BackColor = System.Drawing.Color.OliveDrab;
+            this.panelBet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBet.Controls.Add(this.textBoxPunter3);
+            this.panelBet.Controls.Add(this.textBoxPunter2);
+            this.panelBet.Controls.Add(this.textBoxPunter1);
+            this.panelBet.Controls.Add(this.groupBox1);
+            this.panelBet.Controls.Add(this.punter3Radio);
+            this.panelBet.Controls.Add(this.punter2Radio);
+            this.panelBet.Controls.Add(this.punter1Radio);
+            this.panelBet.Location = new System.Drawing.Point(1, 413);
+            this.panelBet.Name = "panelBet";
+            this.panelBet.Size = new System.Drawing.Size(1480, 256);
+            this.panelBet.TabIndex = 5;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAction.Location = new System.Drawing.Point(209, 693);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(1080, 66);
+            this.btnAction.TabIndex = 6;
+            this.btnAction.Text = "Place Bet";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // punter1Radio
+            // 
+            this.punter1Radio.AutoSize = true;
+            this.punter1Radio.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punter1Radio.Location = new System.Drawing.Point(10, 21);
+            this.punter1Radio.Name = "punter1Radio";
+            this.punter1Radio.Size = new System.Drawing.Size(163, 42);
+            this.punter1Radio.TabIndex = 0;
+            this.punter1Radio.TabStop = true;
+            this.punter1Radio.Text = "Punter 1";
+            this.punter1Radio.UseVisualStyleBackColor = true;
+            this.punter1Radio.CheckedChanged += new System.EventHandler(this.puntersRadio_CheckedChanged);
+            // 
+            // punter2Radio
+            // 
+            this.punter2Radio.AutoSize = true;
+            this.punter2Radio.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punter2Radio.Location = new System.Drawing.Point(10, 102);
+            this.punter2Radio.Name = "punter2Radio";
+            this.punter2Radio.Size = new System.Drawing.Size(163, 42);
+            this.punter2Radio.TabIndex = 1;
+            this.punter2Radio.TabStop = true;
+            this.punter2Radio.Text = "Punter 2";
+            this.punter2Radio.UseVisualStyleBackColor = true;
+            this.punter2Radio.CheckedChanged += new System.EventHandler(this.puntersRadio_CheckedChanged);
+            // 
+            // punter3Radio
+            // 
+            this.punter3Radio.AutoSize = true;
+            this.punter3Radio.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punter3Radio.Location = new System.Drawing.Point(10, 183);
+            this.punter3Radio.Name = "punter3Radio";
+            this.punter3Radio.Size = new System.Drawing.Size(163, 42);
+            this.punter3Radio.TabIndex = 2;
+            this.punter3Radio.TabStop = true;
+            this.punter3Radio.Text = "Punter 3";
+            this.punter3Radio.UseVisualStyleBackColor = true;
+            this.punter3Radio.CheckedChanged += new System.EventHandler(this.puntersRadio_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericCycleNo);
+            this.groupBox1.Controls.Add(this.numericBetAmount);
+            this.groupBox1.Controls.Add(this.labelCycleNo);
+            this.groupBox1.Controls.Add(this.labelBet);
+            this.groupBox1.Controls.Add(this.labelMax);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(877, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 248);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Betting Setting";
+            // 
+            // textBoxPunter1
+            // 
+            this.textBoxPunter1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPunter1.Location = new System.Drawing.Point(189, 23);
+            this.textBoxPunter1.Name = "textBoxPunter1";
+            this.textBoxPunter1.Size = new System.Drawing.Size(682, 42);
+            this.textBoxPunter1.TabIndex = 4;
+            // 
+            // textBoxPunter2
+            // 
+            this.textBoxPunter2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPunter2.Location = new System.Drawing.Point(189, 104);
+            this.textBoxPunter2.Name = "textBoxPunter2";
+            this.textBoxPunter2.Size = new System.Drawing.Size(682, 42);
+            this.textBoxPunter2.TabIndex = 5;
+            // 
+            // textBoxPunter3
+            // 
+            this.textBoxPunter3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPunter3.Location = new System.Drawing.Point(189, 185);
+            this.textBoxPunter3.Name = "textBoxPunter3";
+            this.textBoxPunter3.Size = new System.Drawing.Size(682, 42);
+            this.textBoxPunter3.TabIndex = 6;
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Location = new System.Drawing.Point(131, 41);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(357, 38);
+            this.labelMax.TabIndex = 0;
+            this.labelMax.Text = "Max Bet Amount is $50";
+            // 
+            // labelBet
+            // 
+            this.labelBet.AutoSize = true;
+            this.labelBet.Location = new System.Drawing.Point(37, 102);
+            this.labelBet.Name = "labelBet";
+            this.labelBet.Size = new System.Drawing.Size(286, 38);
+            this.labelBet.TabIndex = 1;
+            this.labelBet.Text = "Bets Of Amount $ ";
+            // 
+            // labelCycleNo
+            // 
+            this.labelCycleNo.AutoSize = true;
+            this.labelCycleNo.Location = new System.Drawing.Point(37, 180);
+            this.labelCycleNo.Name = "labelCycleNo";
+            this.labelCycleNo.Size = new System.Drawing.Size(264, 38);
+            this.labelCycleNo.TabIndex = 2;
+            this.labelCycleNo.Text = "Bets on Cycle No";
+            // 
+            // numericBetAmount
+            // 
+            this.numericBetAmount.Location = new System.Drawing.Point(416, 100);
+            this.numericBetAmount.Name = "numericBetAmount";
+            this.numericBetAmount.Size = new System.Drawing.Size(120, 45);
+            this.numericBetAmount.TabIndex = 3;
+            // 
+            // numericCycleNo
+            // 
+            this.numericCycleNo.Location = new System.Drawing.Point(416, 178);
+            this.numericCycleNo.Name = "numericCycleNo";
+            this.numericCycleNo.Size = new System.Drawing.Size(120, 45);
+            this.numericCycleNo.TabIndex = 4;
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1481, 771);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.panelBet);
+            this.Controls.Add(this.pictureCycle4);
+            this.Controls.Add(this.pictureCycle3);
+            this.Controls.Add(this.pictureCycle2);
+            this.Controls.Add(this.pictureCycle1);
+            this.Controls.Add(this.pictureTracks);
+            this.MaximizeBox = false;
+            this.Name = "HomeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home :: BET GAME";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCycle4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTracks)).EndInit();
+            this.panelBet.ResumeLayout(false);
+            this.panelBet.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBetAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericCycleNo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureCycle1;
+        private System.Windows.Forms.PictureBox pictureCycle2;
+        private System.Windows.Forms.PictureBox pictureCycle3;
+        private System.Windows.Forms.PictureBox pictureCycle4;
+        private System.Windows.Forms.PictureBox pictureTracks;
+        private System.Windows.Forms.Panel panelBet;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.RadioButton punter1Radio;
+        private System.Windows.Forms.RadioButton punter3Radio;
+        private System.Windows.Forms.RadioButton punter2Radio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxPunter3;
+        private System.Windows.Forms.TextBox textBoxPunter2;
+        private System.Windows.Forms.TextBox textBoxPunter1;
+        private System.Windows.Forms.Label labelCycleNo;
+        private System.Windows.Forms.Label labelBet;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.NumericUpDown numericCycleNo;
+        private System.Windows.Forms.NumericUpDown numericBetAmount;
+    }
+}
+
---
+++ BetGame2/RaceEngine/Bet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetGame2.RaceEngine
+{
+    public class Bet
+    {
+        public int Amount;
+
+        public Cycle Cycle;
+
+        public Bet()
+        {
+
+        }
+    }
+}
